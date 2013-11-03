1740442d469a0a8812fc3b1422903c42b8f47b91
Rob McCann <rob@robmccann.co.uk>
2013-11-03 19:05:40 +0000

    Nodejs-dev is incompatible with 0.10 from ppa

--- lib/nodejs.sh
+++ lib/nodejs.sh
@@ -4,5 +4,5 @@ function install_nodejs {
   apt-get -y install python-software-properties
   add-apt-repository ppa:chris-lea/node.js
   apt-get -y update
-  apt-get -y install nodejs nodejs-dev
+  apt-get -y install nodejs
 }
